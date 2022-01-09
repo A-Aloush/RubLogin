b624e8fc79b85fb231987dae75af4f25233add05
A-Aloush <79587775+A-Aloush@users.noreply.github.com>
2022-01-09 18:21:49 +0100

    Add IP auto detection

--- RubLogin.py
+++ RubLogin.py
@@ -1,10 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
+
+ip = 'https://login.ruhr-uni-bochum.de/cgi-bin/start'
+
+b = BeautifulSoup(requests.get(ip).content, "html.parser")
+s = b.find_all(type="ipaddr")
 
 url = 'https://login.ruhr-uni-bochum.de/cgi-bin/laklogin'
 values = {  'code':'1',
             'loginid':'username',
             'password':'password',
-            'ipaddr':'your-IP',
+            'ipaddr':str(s).split("value")[1][2:-4],
             'action':'Login'    }
 
 r = requests.post(url, data=values)
